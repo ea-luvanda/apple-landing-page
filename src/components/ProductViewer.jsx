f8fc133fe6ef0316a408a3430f35d96c825fb2d9
ea-luvanda <e.a.luvanda@gmail.com>
2025-11-27 01:19:35 +0300

    Added the GLB files with the gltfjsx model

--- src/components/ProductViewer.jsx
+++ src/components/ProductViewer.jsx
@@ -2,6 +2,7 @@ import clsx from "clsx";
 import useMacbookStore from "../store";
 import { Canvas } from "@react-three/fiber";
 import { Box, OrbitControls } from "@react-three/drei";
+import MacbookModel14 from "./models/Macbook-14"; 
 
 const ProductViewer = () => {
     const { color, scale, setColor, setScale } = useMacbookStore();
@@ -41,8 +42,8 @@ const ProductViewer = () => {
             </div> 
 
             <Canvas id="canvas" camera={{ position:[0,2,5], fov: 50, near: 0.1, far: 100}}>
-                <Box position={[0,0,0]} scale={10 * scale} material-color={color}/>
-
+                <ambientLight intensity={1}/>
+                <MacbookModel14 scale={scale} position={[0, 0, 0]}/>
                 <OrbitControls enableZoom={false} />
             
             </Canvas>
