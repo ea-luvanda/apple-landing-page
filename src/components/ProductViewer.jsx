bc563c7da4297781b29662da57c19b577309418d
ea-luvanda <e.a.luvanda@gmail.com>
2025-11-27 00:47:42 +0300

    Added three js and its helpers, also added canvsa and orbit controls

--- src/components/ProductViewer.jsx
+++ src/components/ProductViewer.jsx
@@ -1,5 +1,7 @@
 import clsx from "clsx";
 import useMacbookStore from "../store";
+import { Canvas } from "@react-three/fiber";
+import { Box, OrbitControls } from "@react-three/drei";
 
 const ProductViewer = () => {
     const { color, scale, setColor, setScale } = useMacbookStore();
@@ -38,7 +40,12 @@ const ProductViewer = () => {
               </div>
             </div> 
 
-            <p className="text-white text-4xl">Render Canvas</p>
+            <Canvas id="canvas" camera={{ position:[0,2,5], fov: 50, near: 0.1, far: 100}}>
+                <Box position={[0,0,0]} scale={10 * scale} material-color={color}/>
+
+                <OrbitControls enableZoom={false} />
+            
+            </Canvas>
         </section>
     );
 }
