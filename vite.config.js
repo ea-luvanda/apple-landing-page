b27b9db998d9845113157642d01a2017eb3bfb21
ea-luvanda <e.a.luvanda@gmail.com>
2025-11-29 00:01:52 +0300

    Prepare for GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
+  base: '/apple-landing-page/',
   plugins: [react(), tailwindcss()],
 })
