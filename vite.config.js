cbdbecd365d6cd0ddfa67f9a0a5269101d1cfd6c
ea-luvanda <e.a.luvanda@gmail.com>
2025-12-02 03:57:30 +0300

    failed with gh-pages will come back but now lets continue

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base: '/apple-landing-page/',
+  base: process.env.NODE_ENV === 'production' ? '/apple-landing-page/' : '/',
   plugins: [react(), tailwindcss()],
 })
